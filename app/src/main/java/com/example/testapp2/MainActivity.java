39125f9cadf8883480adf3829446e7374cda911d
Sekun05 <sekun524@gmail.com>
2020-01-23 18:16:33 +0900

    QRコードリーダーの仮実装
    * ZXing Android Embedded を埋め込んだだけ

--- app/src/main/java/com/example/testapp2/MainActivity.java
+++ app/src/main/java/com/example/testapp2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.testapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
 import com.stephentuso.welcome.WelcomeHelper;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         welcomeScreen = new WelcomeHelper(this, TutorialActivity.class);
         welcomeScreen.show(savedInstanceState);
 
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -23,4 +31,15 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         welcomeScreen.onSaveInstanceState(outState);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            Toast.makeText(this, "Scanned: " + result.getContents(),Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/testapp2/ReaderActivity.java
@@ -0,0 +1,13 @@
+package com.example.testapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
